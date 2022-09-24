c8acabd623d31d815e50f90e435345b439e26976
yqq <youngqqcn@gmail.com>
2022-09-24 11:10:39 +0800

    moment to ZH-CN

--- apps/block_scout_web/assets/js/lib/from_now.js
+++ apps/block_scout_web/assets/js/lib/from_now.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import moment from 'moment'
 
-defines = {
+let momentDefines = {
     months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
     monthsShort: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
     weekdays: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
@@ -112,7 +112,7 @@ export function updateAllAges ($container = $(document)) {
 }
 function tryUpdateAge (el) {
 
-  moment.locale('zh-cn', defines)
+  moment.locale('zh-cn', momentDefines)
   if (!el.dataset.fromNow) return
 
   const timestamp = moment(el.dataset.fromNow)
