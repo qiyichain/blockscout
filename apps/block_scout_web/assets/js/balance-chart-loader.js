b3b307204d44eabaa04645ec8d0ca2deb8ecf561
yqq <youngqqcn@gmail.com>
2022-09-24 14:20:16 +0800

    remove coin history chart

--- apps/block_scout_web/assets/js/balance-chart-loader.js
+++ apps/block_scout_web/assets/js/balance-chart-loader.js
@@ -4,10 +4,10 @@ import { formatAllUsdValues, updateAllCalculatedUsdValues } from './lib/currency
 import { createCoinBalanceHistoryChart } from './lib/coin_balance_history_chart'
 
 (function () {
-  const coinBalanceHistoryChartElement = $('[data-chart="coinBalanceHistoryChart"]')[0]
-  if (coinBalanceHistoryChartElement) {
-    window.coinBalanceHistoryChart = createCoinBalanceHistoryChart(coinBalanceHistoryChartElement)
-  }
-  formatAllUsdValues()
-  updateAllCalculatedUsdValues()
+//   const coinBalanceHistoryChartElement = $('[data-chart="coinBalanceHistoryChart"]')[0]
+//   if (coinBalanceHistoryChartElement) {
+//     window.coinBalanceHistoryChart = createCoinBalanceHistoryChart(coinBalanceHistoryChartElement)
+//   }
+//   formatAllUsdValues()
+//   updateAllCalculatedUsdValues()
 })()
